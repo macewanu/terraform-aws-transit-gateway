bddea27195d8d68ee0996c0a04010f8d4417434a
Brent Farand <36188155+BFarand@users.noreply.github.com>
2025-02-02 19:22:07 -0700

    fix: typo

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "aws_ec2_transit_gateway" "default" {
   default_route_table_association     = var.default_route_table_association
   default_route_table_propagation     = var.default_route_table_propagation
   dns_support                         = var.dns_support
-  security_group_referencing_support  = var.enable_security_group_referencing_support ? "enable" : " disable"
+  security_group_referencing_support  = var.enable_security_group_referencing_support ? "enable" : "disable"
   vpn_ecmp_support                    = var.vpn_ecmp_support
   tags                                = module.this.tags
   transit_gateway_cidr_blocks         = var.transit_gateway_cidr_blocks
